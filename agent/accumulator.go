4b17b6c0e9cea66edbaf1144b9b18c52f6885f9b
geeksaga <geeksaga@geeksaga.com>
2020-03-15 10:26:52 +0900

    Add agent

---
+++ agent/accumulator.go
@@ -0,0 +1,91 @@
+package agent
+
+import (
+	"geeksaga.com/os/straw/internal"
+	"geeksaga.com/os/straw/metric"
+	"geeksaga.com/os/straw/plugins"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type MetricMaker interface {
+	LogName() string
+	MakeMetric(metric internal.Metric) internal.Metric
+}
+
+type accumulator struct {
+	maker     MetricMaker
+	metrics   chan<- internal.Metric
+	precision time.Duration
+}
+
+func NewAccumulator(
+	maker MetricMaker,
+	metrics chan<- internal.Metric,
+) plugins.Accumulator {
+	acc := accumulator{
+		maker:     maker,
+		metrics:   metrics,
+		precision: time.Nanosecond,
+	}
+	return &acc
+}
+
+func (ac *accumulator) AddFields(
+	measurement string,
+	fields map[string]interface{},
+	tags map[string]string,
+	t ...time.Time,
+) {
+	ac.addFields(measurement, tags, fields, internal.Untyped, t...)
+}
+
+func (ac *accumulator) AddGauge(
+	measurement string,
+	fields map[string]interface{},
+	tags map[string]string,
+	t ...time.Time,
+) {
+	ac.addFields(measurement, tags, fields, internal.Gauge, t...)
+}
+
+func (ac *accumulator) addFields(
+	measurement string,
+	tags map[string]string,
+	fields map[string]interface{},
+	tp internal.ValueType,
+	t ...time.Time,
+) {
+	m, err := metric.New(measurement, tags, fields, ac.getTime(t), tp)
+	if err != nil {
+		return
+	}
+	if m := ac.maker.MakeMetric(m); m != nil {
+		ac.metrics <- m
+	}
+}
+
+// AddError passes a runtime error to the accumulator.
+// The error will be tagged with the plugin name and written to the log.
+func (ac *accumulator) AddError(err error) {
+	if err == nil {
+		return
+	}
+	//ac.maker.Log().Errorf("Error in plugin: %v", err)
+	log.Errorf("Error in plugin: %v", err)
+}
+
+func (ac *accumulator) SetPrecision(precision time.Duration) {
+	ac.precision = precision
+}
+
+func (ac *accumulator) getTime(t []time.Time) time.Time {
+	var timestamp time.Time
+	if len(t) > 0 {
+		timestamp = t[0]
+	} else {
+		timestamp = time.Now()
+	}
+	return timestamp.Round(ac.precision)
+}
---
+++ agent/agent.go
@@ -0,0 +1,466 @@
+package agent
+
+import (
+	"context"
+	"fmt"
+	"geeksaga.com/os/straw/internal"
+	"geeksaga.com/os/straw/internal/config"
+	"geeksaga.com/os/straw/internal/models"
+	"geeksaga.com/os/straw/plugins"
+	"runtime"
+	"sync"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// Agent runs a set of plugins.
+type Agent struct {
+	Config *config.Config
+}
+
+// NewAgent returns an Agent for the given Config.
+func NewAgent(config *config.Config) (*Agent, error) {
+	a := &Agent{
+		Config: config,
+	}
+	return a, nil
+}
+
+// Run starts and runs the Agent until the context is done.
+func (a *Agent) Run(ctx context.Context) error {
+	log.Printf("I! [agent] Config: Interval:%s, Quiet:%#v, Hostname:%#v, "+
+		"Flush Interval:%s",
+		a.Config.Agent.Interval.Duration, a.Config.Agent.Quiet,
+		a.Config.Agent.Hostname, a.Config.Agent.FlushInterval.Duration)
+
+	if ctx.Err() != nil {
+		return ctx.Err()
+	}
+
+	log.Printf("D! [agent] Initializing plugins")
+	err := a.initPlugins()
+	if err != nil {
+		return err
+	}
+
+	log.Printf("D! [agent] Connecting outputs")
+	err = a.connectOutputs(ctx)
+	if err != nil {
+		return err
+	}
+
+	inputC := make(chan internal.Metric, 100)
+
+	startTime := time.Now()
+
+	log.Printf("D! [agent] Starting service inputs")
+	err = a.startServiceInputs(ctx, inputC)
+	if err != nil {
+		return err
+	}
+
+	var wg sync.WaitGroup
+
+	src := inputC
+	dst := inputC
+
+	wg.Add(1)
+	go func(dst chan internal.Metric) {
+		defer wg.Done()
+
+		err := a.runInputs(ctx, startTime, dst)
+		if err != nil {
+			log.Printf("E! [agent] Error running inputs: %v", err)
+		}
+
+		log.Printf("D! [agent] Stopping service inputs")
+		a.stopServiceInputs()
+
+		close(dst)
+		log.Printf("D! [agent] Input channel closed")
+	}(dst)
+
+	src = dst
+
+	wg.Add(1)
+	go func(src chan internal.Metric) {
+		defer wg.Done()
+
+		err := a.runOutputs(startTime, src)
+		if err != nil {
+			log.Printf("E! [agent] Error running outputs: %v", err)
+		}
+	}(src)
+
+	wg.Wait()
+
+	log.Printf("D! [agent] Closing outputs")
+	a.closeOutputs()
+
+	log.Printf("D! [agent] Stopped Successfully")
+	return nil
+}
+
+// runInputs starts and triggers the periodic gather for Inputs.
+//
+// When the context is done the timers are stopped and this function returns
+// after all ongoing Gather calls complete.
+func (a *Agent) runInputs(
+	ctx context.Context,
+	startTime time.Time,
+	dst chan<- internal.Metric,
+) error {
+	var wg sync.WaitGroup
+	for _, input := range a.Config.Inputs {
+		interval := a.Config.Agent.Interval.Duration
+		jitter := a.Config.Agent.CollectionJitter.Duration
+
+		// Overwrite agent interval if this plugin has its own.
+		if input.Config.Interval != 0 {
+			interval = input.Config.Interval
+		}
+
+		acc := NewAccumulator(input, dst)
+		acc.SetPrecision(a.Precision())
+
+		wg.Add(1)
+		go func(input *models.RunningInput) {
+			defer wg.Done()
+
+			if a.Config.Agent.RoundInterval {
+				err := internal.SleepContext(ctx, internal.AlignDuration(startTime, interval))
+				if err != nil {
+					return
+				}
+			}
+
+			a.gatherOnInterval(ctx, acc, input, interval, jitter)
+		}(input)
+	}
+	wg.Wait()
+
+	return nil
+}
+
+// gather runs an input's gather function periodically until the context is done.
+func (a *Agent) gatherOnInterval(
+	ctx context.Context,
+	acc plugins.Accumulator,
+	input *models.RunningInput,
+	interval time.Duration,
+	jitter time.Duration,
+) {
+	defer panicRecover(input)
+
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for {
+		err := internal.SleepContext(ctx, internal.RandomDuration(jitter))
+		if err != nil {
+			return
+		}
+
+		err = a.gatherOnce(acc, input, interval)
+		if err != nil {
+			acc.AddError(err)
+		}
+
+		select {
+		case <-ticker.C:
+			continue
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+// gatherOnce runs the input's Gather function once, logging a warning each interval it fails to complete before.
+func (a *Agent) gatherOnce(
+	acc plugins.Accumulator,
+	input *models.RunningInput,
+	timeout time.Duration,
+) error {
+	ticker := time.NewTicker(timeout)
+	defer ticker.Stop()
+
+	done := make(chan error)
+	go func() {
+		done <- input.Gather(acc)
+	}()
+
+	for {
+		select {
+		case err := <-done:
+			return err
+		case <-ticker.C:
+			log.Printf("W! [agent] [%s] did not complete within its interval", input.LogName())
+		}
+	}
+}
+
+// runProcessors applies processors to metrics.
+func (a *Agent) runProcessors(
+	src <-chan internal.Metric,
+	agg chan<- internal.Metric,
+) error {
+	for metric := range src {
+		metrics := a.applyProcessors(metric)
+
+		for _, metric := range metrics {
+			agg <- metric
+		}
+	}
+
+	return nil
+}
+
+// applyProcessors applies all processors to a metric.
+func (a *Agent) applyProcessors(m internal.Metric) []internal.Metric {
+	metrics := []internal.Metric{m}
+
+	return metrics
+}
+
+func updateWindow(start time.Time, roundInterval bool, period time.Duration) (time.Time, time.Time) {
+	var until time.Time
+	if roundInterval {
+		//until = internal.AlignTime(start, period)
+		//if until == start {
+		//	until = internal.AlignTime(start.Add(time.Nanosecond), period)
+		//}
+	} else {
+		until = start.Add(period)
+	}
+
+	since := until.Add(-period)
+
+	return since, until
+}
+
+// runOutputs triggers the periodic write for Outputs.
+
+// Runs until src is closed and all metrics have been processed.  Will call
+// Write one final time before returning.
+func (a *Agent) runOutputs(
+	startTime time.Time,
+	src <-chan internal.Metric,
+) error {
+	interval := a.Config.Agent.FlushInterval.Duration
+	jitter := a.Config.Agent.FlushJitter.Duration
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	var wg sync.WaitGroup
+	for _, output := range a.Config.Outputs {
+		interval := interval
+		// Overwrite agent flush_interval if this plugin has its own.
+		if output.Config.FlushInterval != 0 {
+			interval = output.Config.FlushInterval
+		}
+
+		jitter := jitter
+		// Overwrite agent flush_jitter if this plugin has its own.
+		if output.Config.FlushJitter != nil {
+			jitter = *output.Config.FlushJitter
+		}
+
+		wg.Add(1)
+		go func(output *models.RunningOutput) {
+			defer wg.Done()
+
+			if a.Config.Agent.RoundInterval {
+				err := internal.SleepContext(ctx, internal.AlignDuration(startTime, interval))
+				if err != nil {
+					return
+				}
+			}
+
+			a.flush(ctx, output, interval, jitter)
+		}(output)
+	}
+
+	for metric := range src {
+		for i, output := range a.Config.Outputs {
+			if i == len(a.Config.Outputs)-1 {
+				output.AddMetric(metric)
+			} else {
+				output.AddMetric(metric.Copy())
+			}
+		}
+	}
+
+	log.Println("I! [agent] Hang on, flushing any cached metrics before shutdown")
+	cancel()
+	wg.Wait()
+
+	return nil
+}
+
+// flush runs an output's flush function periodically until the context is done.
+func (a *Agent) flush(
+	ctx context.Context,
+	output *models.RunningOutput,
+	interval time.Duration,
+	jitter time.Duration,
+) {
+	// since we are watching wo channels we need a ticker with the jitter integrated.
+	ticker := NewTicker(interval, jitter)
+	defer ticker.Stop()
+
+	logError := func(err error) {
+		if err != nil {
+			log.Printf("E! [agent] Error writing to %s: %v", output.LogName(), err)
+		}
+	}
+
+	for {
+		// Favor shutdown over other methods.
+		select {
+		case <-ctx.Done():
+			logError(a.flushOnce(output, interval, output.Write))
+			return
+		default:
+		}
+
+		select {
+		case <-ticker.C:
+			logError(a.flushOnce(output, interval, output.Write))
+		case <-output.BatchReady:
+			// Favor the ticker over batch ready
+			select {
+			case <-ticker.C:
+				logError(a.flushOnce(output, interval, output.Write))
+			default:
+				logError(a.flushOnce(output, interval, output.WriteBatch))
+			}
+		case <-ctx.Done():
+			logError(a.flushOnce(output, interval, output.Write))
+			return
+		}
+	}
+}
+
+// flushOnce runs the output's Write function once, logging a warning each interval it fails to complete before.
+func (a *Agent) flushOnce(
+	output *models.RunningOutput,
+	timeout time.Duration,
+	writeFunc func() error,
+) error {
+	ticker := time.NewTicker(timeout)
+	defer ticker.Stop()
+
+	done := make(chan error)
+	go func() {
+		done <- writeFunc()
+	}()
+
+	for {
+		select {
+		case err := <-done:
+			output.LogBufferStatus()
+			return err
+		case <-ticker.C:
+			log.Printf("W! [agent] [%q] did not complete within its flush interval", output.LogName())
+			output.LogBufferStatus()
+		}
+	}
+}
+
+// initPlugins runs the Init function on plugins.
+func (a *Agent) initPlugins() error {
+	for _, input := range a.Config.Inputs {
+		err := input.Init()
+		if err != nil {
+			return fmt.Errorf("could not initialize input %s: %v",
+				input.LogName(), err)
+		}
+	}
+
+	for _, output := range a.Config.Outputs {
+		err := output.Init()
+		if err != nil {
+			return fmt.Errorf("could not initialize output %s: %v",
+				output.Config.Name, err)
+		}
+	}
+	return nil
+}
+
+// connectOutputs connects to all outputs.
+func (a *Agent) connectOutputs(ctx context.Context) error {
+	for _, output := range a.Config.Outputs {
+		log.Printf("D! [agent] Attempting connection to [%s]", output.LogName())
+		err := output.Output.Connect()
+		if err != nil {
+			log.Printf("E! [agent] Failed to connect to [%s], retrying in 15s, "+
+				"error was '%s'", output.LogName(), err)
+
+			err := internal.SleepContext(ctx, 15*time.Second)
+			if err != nil {
+				return err
+			}
+
+			err = output.Output.Connect()
+			if err != nil {
+				return err
+			}
+		}
+		log.Printf("D! [agent] Successfully connected to %s", output.LogName())
+	}
+	return nil
+}
+
+// closeOutputs closes all outputs.
+func (a *Agent) closeOutputs() {
+	for _, output := range a.Config.Outputs {
+		output.Close()
+	}
+}
+
+// startServiceInputs starts all service inputs.
+func (a *Agent) startServiceInputs(
+	ctx context.Context,
+	dst chan<- internal.Metric,
+) error {
+	return nil
+}
+
+// stopServiceInputs stops all service inputs.
+func (a *Agent) stopServiceInputs() {
+}
+
+// Returns the rounding precision for metrics.
+func (a *Agent) Precision() time.Duration {
+	precision := a.Config.Agent.Precision.Duration
+	interval := a.Config.Agent.Interval.Duration
+
+	if precision > 0 {
+		return precision
+	}
+
+	switch {
+	case interval >= time.Second:
+		return time.Second
+	case interval >= time.Millisecond:
+		return time.Millisecond
+	case interval >= time.Microsecond:
+		return time.Microsecond
+	default:
+		return time.Nanosecond
+	}
+}
+
+// panicRecover displays an error if an input panics.
+func panicRecover(input *models.RunningInput) {
+	if err := recover(); err != nil {
+		trace := make([]byte, 2048)
+		runtime.Stack(trace, true)
+		//log.Printf("E! FATAL: [%s] panicked: %s, Stack:\n%s",
+		//	input.LogName(), err, trace)
+		log.Println("E! PLEASE REPORT THIS PANIC ON GITHUB with " +
+			"stack trace, configuration, and OS information: " +
+			"https://github.com/influxdata/telegraf/issues/new/choose")
+	}
+}
