daf24a4e40119ba618dff83cd8703d9254e32897
albert <albert@jennifersoft.com>
2020-09-02 16:57:11 +0900

    Add System inputs plugin

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -3,6 +3,8 @@ package all
 import (
 	_ "github.com/geekflow/straw/plugins/inputs/cpu"
 	_ "github.com/geekflow/straw/plugins/inputs/disk"
+	_ "github.com/geekflow/straw/plugins/inputs/mem"
+	_ "github.com/geekflow/straw/plugins/inputs/net"
 	_ "github.com/geekflow/straw/plugins/inputs/process"
 	_ "github.com/geekflow/straw/plugins/inputs/procstat"
 )
